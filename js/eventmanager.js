c59953041d8d9c2a29de2ebf92997bf11e006b35
Spirito82 <Spirito82@users.noreply.github.com>
2016-05-04 13:58:45 +0200

    Added files via upload
    added function to loadAnnotations

--- js/eventmanager.js
+++ js/eventmanager.js
@@ -21,50 +21,62 @@ $(function () {
         });
 }); */
         
-        var arr;
-		// funzione caricata all'apertura della pagina
-		$(document).ready(function (e) {
-			var text = $("#data").html();
-			arr = JSON.parse(text);
-			//alert(arr.events[0]['conference']);
-			for(var i = 0; i < arr.events.length; i++)
-				$('#events').append('<li><a href="#" data-id="' + i + '" onclick="selectEvent(this)" >'+arr.events[i]['conference']+'</a></li>');
-		  $("#logBtn").click(function(){
-            $("#logModal").modal();
-		  });
-		});
-        
-		function selectEvent(identifier)
-		{
-			var id = $(identifier).attr("data-id");
-			var submissions = arr.events[id]['submissions'];
-			var tmp = "";
-			for(var i = 0; i < submissions.length; i++)
-				tmp += '<a href="#" class="list-group-item" data-id="' + i +'"'+' data-eventid="' + id + '" onclick="openArticle(this)" >'+submissions[i]['title']+'</a>';
-			$("#articles").html(tmp);
-		}
-		
-        function changeArticle(){
-            $( "#RASHhead" ).empty();
-            $( "#paperdiv" ).empty();
-        }
-      
-		function openArticle(identifier)           
-		{
-            changeArticle();
-            var eventN= $(identifier).attr('data-eventid');
-            var articleN= $(identifier).attr('data-id');
-            var submission = arr.events[eventN]['submissions'];
-            jQuery.ajax({
-                url:submission[articleN]['url'], 
-                success:function( data ) {
-					jQuery.each($(data).filter("section"),function(index,value){$("#paperdiv").append($( this ));});
-					jQuery.each($(data).filter("title"),function(index,value){$("#RASHhead").append($( this ));});
-					jQuery.each($(data).filter("meta"),function(index,value){$("#RASHhead").append($( this ));});
-					jQuery.each($(data).filter("#voti"),function(index,value){$("#RASHhead").append($(this ));});
-					$("#RASHhead").append('<script src="js/rash.js"> </script>');
-                },
-                async: true
-            });
-		}
+var arr;
+// funzione per caricare la lista Eventi all'apertura della pagina
+$(document).ready(function (e) {
+	var text = $("#data").html();
+	arr = JSON.parse(text);
+	//alert(arr.events[0]['conference']);
+	for(var i = 0; i < arr.events.length; i++)
+		$('#events').append('<li><a href="#" data-id="' + i + '" onclick="selectEvent(this)" >'+arr.events[i]['conference']+'</a></li>');
+  $("#logBtn").click(function(){
+	$("#logModal").modal();
+  });
+});
+
+function selectEvent(identifier)
+{
+	var id = $(identifier).attr("data-id");
+	var submissions = arr.events[id]['submissions'];
+	var tmp = "";
+	for(var i = 0; i < submissions.length; i++)
+		tmp += '<a href="#" class="list-group-item" data-id="' + i +'"'+' data-eventid="' + id + '" onclick="openArticle(this)" >'+submissions[i]['title']+'</a>';
+	$("#articles").html(tmp);
+}
+
+function changeArticle(){
+	$( "#RASHhead" ).empty();
+	$( "#paperdiv" ).empty();
+}
+
+function openArticle(identifier)
+{
+	changeArticle();
+	var eventN= $(identifier).attr('data-eventid');
+	var articleN= $(identifier).attr('data-id');
+	var submission = arr.events[eventN]['submissions'];
+	jQuery.ajax({
+		url:submission[articleN]['url'], 
+		success:function( data ) {
+			jQuery.each($(data).filter("section"),function(index,value){$("#paperdiv").append($( this ));});
+			jQuery.each($(data).filter("title"),function(index,value){$("#RASHhead").append($( this ));});
+			jQuery.each($(data).filter("meta"),function(index,value){$("#RASHhead").append($( this ));});
+			jQuery.each($(data).filter("#voti"),function(index,value){$("#RASHhead").append($(this ));});
+			$("#RASHhead").append('<script src="js/rash.js"> </script>');
+			loadAnnotation(data);
+		},
+		async: true
+	});
+}
+
+//funzione per caricare le annotazioni del documento
+function loadAnnotation(data){
+	var text = $(data).filter("#data").html();
+	//load Annotations
+	var arr = JSON.parse(text);
+	for(var i=0; i < arr.length; i++){			
+		$(arr[i]['ref']).attr('class', arr[i]['@type']);
+		$(arr[i]['ref']).css('background-color','yellow');
+	}
+}
         
