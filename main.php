f108a11274db22a633f69c01ed1b6a07776a3101
frabaddo <paradisecity93@gmail.com>
2016-07-13 19:32:45 +0200

    Update main.php

--- main.php
+++ main.php
@@ -128,9 +128,18 @@
 					</div>
 				</nav>
 				<!-- PAPERDIV !-->
-				<div id="RASHhead"></div>
+				<div id="RASHhead">
+				</div>
 				<div id="paperdiv" class="col-sm-9 col-md-9 col-lg-9" onmouseup="Highlight()">   
-                </div>
+                		</div>
+                		<div id="accessorybutton">
+                    			<div class="tomenu list-group-item list-group-item-menu">
+                        			<span onclick="toX('annotationsMenu')" class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                    			</div>
+                    			<div class="tomenu list-group-item list-group-item-menu">
+                        			<span onclick='toX("articles")' class="glyphicon glyphicon-file" aria-hidden="true"></span>
+                    			</div>
+                		</div>
 			</div>
 			<!-- Modals -->
 			<div id="createAnnotation" class="modal fade" role="dialog">
